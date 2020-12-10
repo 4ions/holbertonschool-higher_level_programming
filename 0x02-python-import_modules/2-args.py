f9d71dfb0e1f334d3909f2603aef7f15f3318c2c
4ions <leovalenciasantana@gmail.com>
2020-12-10 16:25:25 +0000

    Fixed error code should not be executed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-size = len(sys.argv)
-if size - 1 == 0:
-    print("{} arguments.".format(size - 1))
-else:
-    if size - 1 == 1:
-        print("{} argument:".format(size - 1))
+if __name__ == "__main__":
+    size = len(sys.argv)
+    if size - 1 == 0:
+        print("{} arguments.".format(size - 1))
     else:
-        print("{} arguments:".format(size - 1))
-    for i in range(1, size):
-        print("{}: {}".format(i, sys.argv[i]))
+        if size - 1 == 1:
+            print("{} argument:".format(size - 1))
+        else:
+            print("{} arguments:".format(size - 1))
+        for i in range(1, size):
+            print("{}: {}".format(i, sys.argv[i]))
