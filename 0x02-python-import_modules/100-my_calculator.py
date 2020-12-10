c16241eabd0d8b7037eb95a9c911fcea214b23e8
4ions <leovalenciasantana@gmail.com>
2020-12-10 18:24:31 +0000

    Fixed some errores

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-import sys
+from sys import exit, argv
 if __name__ == "__main__":
-    argv = sys.argv
+    argv = argv
     size = len(argv)
     if size - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <>")
-        sys.exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
+        exit(1)
+    
+    a = int(argv[1])
+    b = int(argv[3])
 
     if argv[2] == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif argv[2] == "-":
-        print("{} - {} = {}".format(a, b, sub(a, b)))
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     elif argv[2] == "*":
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     elif argv[2] == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Avalible operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
