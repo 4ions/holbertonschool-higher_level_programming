6210ad2fee8867459418798dcff52b5db29ce5bc
4ions <leovalenciasantana@gmail.com>
2020-12-10 18:26:21 +0000

    Fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     argv = argv
     size = len(argv)
     if size - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     
     a = int(argv[1])
