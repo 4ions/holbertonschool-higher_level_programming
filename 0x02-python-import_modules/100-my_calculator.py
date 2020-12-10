98ed638de329ef6e89aa4c477d44cf062acc9d83
4ions <leovalenciasantana@gmail.com>
2020-12-10 19:01:46 +0000

    Fixed

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ from calculator_1 import add, sub, mul, div
 from sys import exit, argv
 if __name__ == "__main__":
     size = len(argv)
-    if size -1 != 3:
+    if size - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
