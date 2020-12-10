bee7289cde1666f6128ab072bdc8dd098c2a5d46
4ions <leovalenciasantana@gmail.com>
2020-12-10 18:30:28 +0000

    Fixed whitespace

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if size - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    
+
     a = int(argv[1])
     b = int(argv[3])
 
