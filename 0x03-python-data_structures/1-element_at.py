e84b6d589b242b464b381ec0ed7f03ca9a5c23e6
4ions <leovalenciasantana@gmail.com>
2020-12-15 17:46:49 +0000

    Added -1 and size

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    size = len(my_list)
+    size = len(my_list) - 1
     if idx < 0:
-        return (None)
-    if idx > size:
-        return (None)
+        return None
+    if idx >= size:
+        return None
     return (my_list[idx])
