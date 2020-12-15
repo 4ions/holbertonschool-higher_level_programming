96bcd084471091523d2c260a6c245e2f826847d9
4ions <leovalenciasantana@gmail.com>
2020-12-15 19:45:43 +0000

    other way to make

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,8 @@ def no_c(my_string):
     copy = ""
     for i in my_string:
         if i not in 'Cc':
+            # "not int"" check if in my_string have 'Cc' and
+            # "copy + i" concatenate each letters in copy
             copy = copy + i
 
     return (copy)
