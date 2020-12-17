298a84d008afe5a032351f0134c4abbd9132a4c0
4ions <leovalenciasantana@gmail.com>
2020-12-17 20:35:37 +0000

    Function that translate roman numbers to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    for i in range(len(roman_string)):
+        if roman_string[i] == "I":
+                try:
+                    if roman_string[i + 1] in 'VXLCDM':
+                        result -= 1
+                    else:
+                        result += 1
+                except:
+                    result += 1
+        if roman_string[i] == "V":
+            result += 5
+        if roman_string[i] == "X":
+            result += 10
+        if roman_string[i] == "L":
+            result += 50
+        if roman_string[i] == "C":
+            result += 100
+        if roman_string[i] == "D":
+            result += 500
+        if roman_string[i] == "M":
+            result += 1000
+    return(result)
