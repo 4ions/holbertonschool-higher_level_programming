3d6a57e025be41e0cd0d31ba0147a5b77ee56798
4ions <leovalenciasantana@gmail.com>
2021-01-19 16:35:36 -0500

    Square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Module of inheritance """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class of Rectangle """
+
+    def __init__(self, width, height):
+        """ Initialize new object of rectangle class """
+
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return "[{}] {:d}/{:d}".format(type(self).__name__, self.__width,
+                                       self.__height)
+
+
+class Square(Rectangle):
+    """ Class that inheriting from a rectangle class """
+    def __init__(self, size):
+        """ Initializes Square """
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
