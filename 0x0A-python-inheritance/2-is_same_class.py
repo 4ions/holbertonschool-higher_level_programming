e2fa8e183094ba36b93878406a7df899c7cf1559
4ions <leovalenciasantana@gmail.com>
2021-01-19 14:23:00 -0500

    Return true or false if obj is exactly with class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module of function that return true if the object is exactly """
+
+
+def is_same_class(obj, a_class):
+    """ Return if object is exactly with class """
+    return(type(obj) == a_class)
