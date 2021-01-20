603cba761c042c8c1b32ab9482cf53c011dd5f3c
4ions <leovalenciasantana@gmail.com>
2021-01-20 17:28:08 -0500

    To json to string

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module writes an object to a text file """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Json to string """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
