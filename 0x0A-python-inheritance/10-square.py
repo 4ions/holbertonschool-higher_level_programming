ce12902fdf30c6693f51ed73662ebae4a332a367
4ions <leovalenciasantana@gmail.com>
2021-01-19 21:01:47 -0500

    change super

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,19 +5,19 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ Class of Rectangle """
-
     def __init__(self, width, height):
         """ Initialize new object of rectangle class """
-
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__height = height
         self.__width = width
 
     def area(self):
+        """ Area """
         return self.__height * self.__width
 
     def __str__(self):
+        """ STR """
         return "[{}] {:d}/{:d}".format("Rectangle", self.__width,
                                        self.__height)
 
