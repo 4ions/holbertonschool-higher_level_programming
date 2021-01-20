3f0beab268d39cc8424d3ae91e34b2a9f544ade2
4ions <leovalenciasantana@gmail.com>
2021-01-19 21:28:39 -0500

    Ajum

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Module of inheritance """
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -10,6 +12,7 @@ class Square(Rectangle):
         """ Initializes Square """
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """ Area """
