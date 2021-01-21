c78f26ca3478e09095252d607de44a97aeb3d484
4ions <leovalenciasantana@gmail.com>
2021-01-20 22:51:43 -0500

    Triangule pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Create a pascal triangle """
+
+
+def pascal_triangle(n):
+    """ Create a pascal tringule and return in list """
+
+    if n <= 0:
+        return ([])
+
+    pascal_list = []
+    for i in range(0, n):
+        pascal_list.append([])
+        pascal_list[i].append(1)
+        for j in range(0, i):
+            pascal_list[i].append
+            (pascal_list[i - 1][j - 1] + pascal_list[i - 1][j])
+
+    return pascal_list
