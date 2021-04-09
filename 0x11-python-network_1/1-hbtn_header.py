23fc9996046f5ee68dae1085dd6bf3ef129e80e1
4ions <leovalenciasantana@gmail.com>
2021-04-09 14:36:37 -0500

    Added verification

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" Module of urllib """ 
+""" Module of urllib """
 from urllib import request
 from sys import argv
 
 url = argv[1]
-with request.urlopen(url) as response:
-    html = response.getheader("X-Request-Id")
-    print(html)
+if __name__ == "__main__":
+    with request.urlopen(url) as response:
+        html = response.getheader("X-Request-Id")
+        print(html)
