87f258acff1a4f2787314d2a3017c26b99d2eac0
4ions <leovalenciasantana@gmail.com>
2021-04-09 15:57:46 -0500

    using .get for dictionary

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,4 @@ import sys
 
 if __name__ == '__main__':
     req = requests.get(sys.argv[1])
-    for key, value in req.headers.items():
-        if key == 'X-Request-Id':
-            print(value)
+    print(req.headers.get('X-Request-Id'))
