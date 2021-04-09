b86734f69923392e2494984f5d2945fd0d87cbd8
4ions <leovalenciasantana@gmail.com>
2021-04-09 14:06:21 -0500

    Fixed pep8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import urllib.request
 """ Module of request"""
+import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   html = response.read()
-print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(html), html, html.decode('utf8')))
+    html = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(html), html, html.decode('utf8')))
