879dc49ed01b081d0815d929e4d400c3b3746fc9
4ions <leovalenciasantana@gmail.com>
2021-04-09 15:46:26 -0500

    Hbtn status with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,5 +4,5 @@ import requests
 
 if __name__ == '__main__':
     req = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t -type: {}\n\t- content: {}"
+    print("Body response:\n\t - type: {}\n\t- content: {}"
           .format(type(req.text), req.text))
